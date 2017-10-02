c5d9d141d037f5f82d19db4e83c88a14c065e010
BinaryHound <lneuwirth@mac.com>
2017-10-02 17:49:05 -0600

    First Commit!

---
+++ src/evaluatinofexpressions/EvaluationOfExpressions.java
@@ -0,0 +1,19 @@
+package evaluatinofexpressions;
+import java.util.Stack;
+
+/**
+ *
+ * @author The Genius
+ */
+public class EvaluationOfExpressions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}

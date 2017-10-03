530784d449b84a00b4f12c7b18a4a64536d6e0a0
BinaryHound <lneuwirth@mac.com>
2017-10-02 18:35:28 -0600

    Created a new Method to help process the operands and operators.

--- src/evaluatinofexpressions/EvaluationOfExpressions.java
+++ src/evaluatinofexpressions/EvaluationOfExpressions.java
@@ -33,11 +33,52 @@ public class EvaluationOfExpressions {
         //Preparation for extraction of operands.
         expression = insertBlanks(expression);
         
+        String[] tokens = expression.split(" ");
         
+        for(String token: tokens){
+            if(token.length() == 0){
+                continue;
+            }else if(token.charAt(0) == '+' || token.charAt(0) == '-'){
+                while(!operatorStack.isEmpty() &&
+                      (operatorStack.peek() == '+' ||
+                       operatorStack.peek() == '-' ||
+                       operatorStack.peek() == '*' ||
+                       operatorStack.peek() == '/' )){
+                    processAnOperator(operandStack, operatorStack);
+                }
+            }else if(token.charAt(0) == '*' || token.charAt(0) == '/'){
+                while(!operatorStack.isEmpty() && 
+                      (operatorStack.peek() == '*' ||
+                       operatorStack.peek() == '/')){
+                    processAnOperator(operandStack, operatorStack);
+                }
+                operatorStack.push(token.charAt(0));
+            }else if(token.trim().charAt(0) == '('){
+                operatorStack.push('(');
+            }else if(token.trim().charAt(0) == ')'){
+                while(operatorStack.peek() != '('){
+                    processAnOperator(operandStack, operatorStack);
+                }
+                operandStack.pop();
+            }else {
+                operandStack.push(new Integer(token));
+            }
+        }
+        while(!operatorStack.isEmpty()){
+            processAnOperator(operandStack, operatorStack);
+        }
         
-        return 1;
+        return operandStack.pop();
     }
     
+    public static void processAnOperator(Stack<Integer> operandStack, Stack<Character> operatorStack){
+        char op = operatorStack.pop();
+        int op1 = operandStack.pop();
+        int op2 = operandStack.pop();
+        switch(op){
+            
+        }
+    }
     
     public static String insertBlanks(String s){
         
